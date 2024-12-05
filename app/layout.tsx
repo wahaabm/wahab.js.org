83e13d1fb4510f4e25e3072e63842b42179462c6
WahabMalik414 <malikwahab34@gmail.com>
2024-12-05 17:59:10 +0500

    Remove unused Analytics and SpeedInsights components from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import { Navbar } from "./components/nav";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import Footer from "./components/footer";
 import { ThemeProvider } from "./components/theme-switch";
 import { metaData } from "./config";
@@ -84,8 +82,6 @@ export default function RootLayout({
                         <Navbar />
                         {children}
                         <Footer />
-                        <Analytics />
-                        <SpeedInsights />
                     </main>
                 </ThemeProvider>
             </body>
