31022d887b00eaf9372c121994923c66b8362bf2
WahabMalik414 <malikwahab34@gmail.com>
2024-12-05 21:50:30 +0500

    Resume updated

--- next.config.js
+++ next.config.js
@@ -3,38 +3,6 @@ const nextConfig = {
     output: "export",
     reactStrictMode: true,
     swcMinify: true,
-    async rewrites() {
-        return [
-            {
-                source: "/rss.xml",
-                destination: "/feed/rss.xml",
-            },
-            {
-                source: "/atom.xml",
-                destination: "/feed/atom.xml",
-            },
-            {
-                source: "/feed.json",
-                destination: "/feed/feed.json",
-            },
-            {
-                source: "/rss",
-                destination: "/feed/rss.xml",
-            },
-            {
-                source: "/feed",
-                destination: "/feed/rss.xml",
-            },
-            {
-                source: "/atom",
-                destination: "/feed/atom.xml",
-            },
-            {
-                source: "/json",
-                destination: "/feed/feed.json",
-            },
-        ];
-    },
 };
 
 module.exports = nextConfig;
